23a895f11ca6bc4a3c231086a26193af75637980
Nuwan Goonasekera <2070605+nuwang@users.noreply.github.com>
2019-04-16 21:17:34 +0530

    Remove extra brackets in init script

--- cloudlaunch-server/scripts/0_init_container.sh
+++ cloudlaunch-server/scripts/0_init_container.sh
@@ -4,7 +4,7 @@ echo "Apply database migrations from `pwd`"
 /app/venv/bin/python manage.py migrate
 # Save last error
 errcode=$?
-if [[ $errcode -eq 0 ]]; then
+if [ $errcode -eq 0 ]; then
     echo "Migrations successfully applied"
 else
     echo "Failed to apply migrations. Quitting..."
@@ -17,7 +17,7 @@ echo "Load initial data from /app/initial_data/*.json"
 echo "Create a superuser"
 cat /app/scripts/create_superuser.py | /app/venv/bin/python manage.py shell
 errcode=$?
-if [[ $errcode -eq 0 ]]; then
+if [ $errcode -eq 0 ]; then
     echo "Successfully created the superuser"
 else
     echo "Error creating superuser. Quitting..."
